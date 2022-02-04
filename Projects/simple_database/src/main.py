44a18a179d8ebe1fece3a865f66eb032cf9b0128
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-04 11:59:01 +0200

    add data into the table using the createData function

--- Projects/simple_database/src/main.py
+++ Projects/simple_database/src/main.py
@@ -7,13 +7,19 @@ from timeit import default_timer
 import sqlite3 as db
 
 
+def average(array):
+    summ = sum(array)
+    return float(summ / len(array))
+
+
 class DB:
     DATA_Directory = '../db/'
 
-    def __init__(self, database_name):
+    def __init__(self, database_name, table_name):
         self.dbdir = DB.DATA_Directory
         self.dbFile = database_name
         self.dbPath = f'{DB.DATA_Directory}{database_name}'
+        self.tableName = table_name
 
     def createConnection(self):
         try:
@@ -31,13 +37,14 @@ class DB:
             return 1
         return -1
 
-    def createTable(self, table_name):
+    def createTable(self):
         # check first if the database exists in the proper directory
         if self.checkDatabaseExists(self.dbFile) != 1:
-            return
+            db_object = self.createConnection()
+        else:
+            db_object = self.createConnection()
 
         # establish connection with the database and return the full connection object and the connection status
-        db_object = self.createConnection()
 
         # stop of there are issues with the database connection
         if db_object["status"] == -1:
@@ -50,14 +57,35 @@ class DB:
         cursor = db_conn.cursor()
 
         # perform the creation of the actual table
-        cursor.execute(f''' CREATE TABLE IF NOT EXISTS {table_name}
-                            (array str, size int,average float)''')
+        cursor.execute(f''' CREATE TABLE IF NOT EXISTS {self.tableName}
+                            (array text, size int,average real)''')
         db_conn.commit()
+
+        return db_object
+
+    def addData(self, data):
+        db_object = self.createTable()
+
+        # prepare the data that will be added in the table
+        data = data[0]
+        avg = average(data)
+
+        # open the database connection
+        db_conn = db_object["conn"]
+        # initialize the cursor
+        cursor = db_conn.cursor()
+
+        for _ in range(3):
+            cursor.execute(
+                f'INSERT INTO {self.tableName} VALUES (\f\'{data}\',{len(data)},{avg})')
+        db_conn.commit()
+        db_conn.close()
 
 
 def main():
-    local_db = DB('test_DataBae.db')
-    local_db.createTable('Arrays')
+    local_db = DB('test_DataBae.db', 'Arrays')
+    data = [[1, 2, 3] for _ in range(3)]
+    local_db.addData(data)
 
 
 if __name__ == "__main__":
