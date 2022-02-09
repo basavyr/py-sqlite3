18372520d02e24145c3ee0fb1b955654420ae467
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-09 22:11:00 +0200

    add os picker and random template tools

--- Projects/cloud_scf_db/src/message.py
+++ Projects/cloud_scf_db/src/message.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 
+from random import randrange
+import random
+
+lib_maker = lambda n: [f'lib{idx+1}' for idx in range(n)]
+app_maker = lambda n: [f'app{idx+1}' for idx in range(n)]
+
 
 class Message:
     @staticmethod
     def GenerateTemplateRequest():
-        os = 'macOS'
-        lib_tools = ['lib1', 'lib2', 'lib3']
-        apps = ['app1', 'app2', 'app3']
+        os_list = ['macOS', 'linux', 'Windows']
+        os = random.choice(os_list)
+        n_libs = randrange(1, 5)
+        n_apps = randrange(1, 5)
+        lib_tools = lib_maker(n_libs)
+        apps = app_maker(n_apps)
         timestamp = str(datetime.utcnow())
 
         return (os, lib_tools, apps, timestamp)
