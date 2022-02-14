6b34cf5230b3c82c40bf9e8d447c0e3864a26d31
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-14 20:14:52 +0200

    add async loop for test

---
+++ Projects/async_publisher/main.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import time
+
+host0 = 'broker.hivemq.com'
+
+
+def main():
+    client1 = mqtt.Client('publihser')
+
+    message = 'This is a message'
+
+    print('this is before async connection')
+
+    client1.connect_async(host=host0)
+
+    print('this is after async connection')
+    print('starting the loop')
+    client1.loop_start()
+    time.sleep(3)
+
+    client1.loop_stop()
+
+
+if __name__ == '__main__':
+    main()
