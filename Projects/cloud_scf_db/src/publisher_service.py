3ba6269a623956892679e7acb0d3046408314f9b
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-17 16:33:49 +0200

    add async procedures for subscribing and publishing

--- Projects/cloud_scf_db/src/publisher_service.py
+++ Projects/cloud_scf_db/src/publisher_service.py
@@ -1,7 +1,22 @@
 import mqtt_lib
 import message
+import asyncio
 
-temp_data = message.data_maker(100)
 
-mqtt_pub = mqtt_lib.MQTT_Publish('publisher', 'mngmtRequests')
-mqtt_pub.PublishMessages_Async(temp_data, break_amount=1, verbose=0)
+async def publish_async(data_size, break_amount, verbose):
+    """
+    - publishes data to a topic via connection to a broker
+    - connection is established asynchronously
+    - standard name for topic and publisher ID
+    """
+    temp_data = message.data_maker(data_size)
+
+    try:
+        mqtt_pub = mqtt_lib.MQTT_Publish('publisher', 'mngmtRequests')
+        mqtt_pub.PublishMessages_Async(
+            temp_data, break_amount=break_amount, verbose=verbose)
+    except Exception as error:
+        print(
+            f'Could not publish messages on topic mngmtRequests\nIssue -> {error}')
+    else:
+        return 1
--- Projects/cloud_scf_db/src/subscriber_service.py
+++ Projects/cloud_scf_db/src/subscriber_service.py
@@ -1,4 +1,14 @@
 import mqtt_lib
+import asyncio
 
-mqtt_sub = mqtt_lib.MQTT_Subscribe('subscriber', topic='mngmtRequests')
-mqtt_sub.Subscribe(break_amount=5)
+
+async def subscribe_async(break_amount):
+
+    try:
+        mqtt_sub = mqtt_lib.MQTT_Subscribe('subscriber', topic='mngmtRequests')
+        mqtt_sub.Subscribe(break_amount=break_amount)
+    except Exception as error:
+        print(
+            f'There was an issue while subscription was ON\nIssue -> {error}')
+    else:
+        return 1
